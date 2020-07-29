f0db306c103cb868028df52ffdb8c665b090a2fa
user160244980349 <mkuznetsov7991@gmail.com>
2020-07-29 16:11:18 +0000

    More performant version of lockscreen

--- .config/sway/lock.sh
+++ .config/sway/lock.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-$(rm $HOME/.config/sway/lock-edp.png; \
-grim -o eDP-1    $HOME/.config/sway/lock-edp-raw.png; \
-ffmpeg -loglevel warning -f image2 -i $HOME/.config/sway/lock-edp-raw.png \
-       -filter "boxblur=2" -vframes 1 $HOME/.config/sway/lock-edp.png &) || \
-$(rm $HOME/.config/sway/lock-hdmi.png; \
-grim -o HDMI-A-1 $HOME/.config/sway/lock-hdmi-raw.png; \
-ffmpeg -loglevel warning -f image2 -i $HOME/.config/sway/lock-hdmi-raw.png \
-       -filter "boxblur=2" -vframes 1 $HOME/.config/sway/lock-hdmi.png &); \
+$(rm $HOME/.config/sway/lock-edp.png;
+  grim   -o eDP-1 $HOME/.config/sway/lock-edp-raw.png;
+  ffmpeg -loglevel warning -f image2 -i $HOME/.config/sway/lock-edp-raw.png \
+         -filter "boxblur=3" -vframes 1 $HOME/.config/sway/lock-edp.png) &
+
+$(rm $HOME/.config/sway/lock-hdmi.png;
+  grim   -o HDMI-A-1 $HOME/.config/sway/lock-hdmi-raw.png;
+  ffmpeg -loglevel warning -f image2 -i $HOME/.config/sway/lock-hdmi-raw.png \
+         -filter "boxblur=3" -vframes 1 $HOME/.config/sway/lock-hdmi.png) &
+
+wait $(jobs -p)
+
 swaylock -f -u -i    eDP-1:$HOME/.config/sway/lock-edp.png \
                -i HDMI-A-1:$HOME/.config/sway/lock-hdmi.png
